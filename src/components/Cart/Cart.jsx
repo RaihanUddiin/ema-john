97f5dc677a9a9d4c8efc61144349a0d58fea7ff1
RaihanUddin <raihan.uddiin@gmail.com>
2023-05-05 13:02:08 +0600

    cart from localstorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,19 +7,22 @@ const Cart = ({cart}) => {
 
     let total=0;
     let shippingCharge=0;
+    let quantity=0;
 
     for(const product of cart){
-        total=total+ product.price;
-        shippingCharge=shippingCharge + product.shipping;
+        product.quantity=product.quantity || 1;
+        total=total+ (product.price*product.quantity);
+        shippingCharge=shippingCharge + (product.shipping*product.quantity);
+        quantity=quantity+ product.quantity;
     }
     const tax=((total*7)/100);
-    const grandTotal=total + shippingCharge + tax;
+    const grandTotal=total + shippingCharge + (tax);
 
     
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected items : {cart.length}</p>
+            <p>Selected items : {quantity}</p>
             <p>Total Price : {total}</p>
             <p>Total Shipping Charge : {shippingCharge}</p>
             <p>Total Tax : {tax.toFixed(2)}</p>
